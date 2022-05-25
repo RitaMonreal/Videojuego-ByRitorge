09dcc2c7ddf2b161190a5d7b3987c9aa809e018f
Jorge Acosta <canelitajr13@gmail.com>
2022-05-25 09:47:28 -0500

    Add virus movement

--- Level1.java
+++ Level1.java
@@ -6,5 +6,17 @@ public class Level1 extends World
     {
         super(900, 500, 1); 
         addObject (new CellCharacter(),450,150);
+        createVirus(20);
+    }
+    
+    public void createVirus(int quantity) {
+        int x, y;
+        
+        for(int i = 0; i < quantity; i++) {
+            VirusCharacter virus = new VirusCharacter();
+            x = Greenfoot.getRandomNumber(getWidth());
+            y = Greenfoot.getRandomNumber(getHeight());
+            addObject(virus, x, y);
+        }
     }
 }
