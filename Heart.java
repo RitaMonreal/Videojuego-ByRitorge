446edea09753c26ffb5da8009364b78b953e6874
RitaMonreal <A326347@alumnos.uaslp.mx>
2022-05-28 17:50:21 -0500

    Se sube la nueva modificacion del tamaño de corazon .java

--- Heart.java
+++ Heart.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Heart extends Actor
+public class Heart extends Status
 {
     /**
      * Act - do whatever the Heart wants to do. This method is called whenever
