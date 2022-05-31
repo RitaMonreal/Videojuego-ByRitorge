809bf45e7875fef20b45d2c441dfd795e7bec8d4
RitaMonreal <A326347@alumnos.uaslp.mx>
2022-05-30 19:46:48 -0500

    se atendió la recomnedacion de la creacion de objetos del nivel

--- Level1.java
+++ Level1.java
@@ -32,26 +32,12 @@ public class Level1 extends General
      */
     private void prepare()
     {
-        Donut donut = new Donut();
-        addObject(donut,109,64);
-        Donut donut2 = new Donut();
-        addObject(donut2,206,380);
-        Donut donut3 = new Donut();
-        addObject(donut3,656,164);
-        Donut donut4 = new Donut();
-        addObject(donut4,544,447);
-        Donut donut5 = new Donut();
-        addObject(donut5,458,268);
-        Donut donut6 = new Donut();
-        addObject(donut6,833,413);
-        removeObject(donut6);
-        donut3.setLocation(658,164);
-        removeObject(donut3);
-        Donut donut7 = new Donut();
-        addObject(donut7,864,80);
-        Donut donut8 = new Donut();
-        addObject(donut8,860,477);
-        Donut donut9 = new Donut();
-        addObject(donut9,30,290);
+        addObject(new Donut(),109,64);
+        addObject(new Donut(),206,380);
+        addObject(new Donut(),544,447);
+        addObject(new Donut(),458,268);
+        addObject(new Donut(),864,80);
+        addObject(new Donut(),860,477);
+        addObject(new Donut(),30,290);
     }
 }
