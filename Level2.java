1449b81caea1b96ad2013eba2fd672b884bae1ad
RitaMonreal <A326347@alumnos.uaslp.mx>
2022-05-30 20:11:22 -0500

    limpieza de código

--- Level2.java
+++ Level2.java
@@ -4,98 +4,53 @@ import java.util.List;
 
 public class Level2 extends General
 {
-   
+    public Level2() 
+    {    
+        NextLevelImplementation();
+        prepare();
+    }
+    
     public void NextLevelImplementation(){
         addObject(new NextLevel(), 890, 250);
     }
 
     private void prepare()
     {
-        Ground ground = new Ground();
-        addObject(ground,105,488);
-        Ground ground2 = new Ground();
-        addObject(ground2,321,488);
-        Ground ground3 = new Ground();
-        addObject(ground3,536,488);
-        Ground ground4 = new Ground();
-        addObject(ground4,754,488);
-        Ground ground5 = new Ground();
-        addObject(ground5,882,488);
-
-        Ground ground7 = new Ground();
-        addObject(ground7,420,340);
-
-        Ground ground8 = new Ground();//nivel 1 3
-        addObject(ground8,790,420);
-
-        Ground ground9 = new Ground();//Ultimo izquierda
-        addObject(ground9,110,170);
-        Ground ground11 = new Ground();
-        addObject(ground11,315,170);
-
-        Ground ground10 = new Ground();//El que está antes de caer
-        addObject(ground10,635,274);
-
-        Ground ground6 = new Ground();
-        addObject(ground6,230,386);
-
-        HomoSapines homoSapines = new HomoSapines();
-        addObject(homoSapines,50,350);
-        Fire fire = new Fire();
-        addObject(fire,37,123);
-        Fire fire2 = new Fire();
-        addObject(fire2,352,447);
-        Tool tool = new Tool();
-        addObject(tool,158,349);
-        Tool tool2 = new Tool();
-        addObject(tool2,223,215);
-        Tool tool3 = new Tool();
-        addObject(tool3,284,349);
-        Tool tool4 = new Tool();
-        addObject(tool4,235,349);
-        Tool tool5 = new Tool();
-        addObject(tool5,405,450);
-        Tool tool6 = new Tool();
-        addObject(tool6,629,448);
-        Tool tool7 = new Tool();
-        addObject(tool7,94,129);
-        Tool tool8 = new Tool();
-        addObject(tool8,638,396);
-        Tool tool9 = new Tool();
-        addObject(tool9,582,449);
-        Tool tool10 = new Tool();
-        addObject(tool10,587,394);
-        Tool tool11 = new Tool();
-        addObject(tool11,533,447);
-        Tool tool12 = new Tool();
-        addObject(tool12,490,400);
-        tool12.setLocation(514,389);
-        tool12.setLocation(538,388);
-        Tool tool13 = new Tool();
-        addObject(tool13,470,449);
-        Tool tool14 = new Tool();
-        addObject(tool14,491,395);
-        Tool tool15 = new Tool();
-        addObject(tool15,431,395);
-        Tool tool16 = new Tool();
-        addObject(tool16,200,349);
-        Fire fire3 = new Fire();
-        addObject(fire3,559,332);
-        Tool tool17 = new Tool();
-        addObject(tool17,143,135);
-        Tool tool18 = new Tool();
-        addObject(tool18,193,133);
-        Tool tool19 = new Tool();
-        addObject(tool19,241,136);
-        Fire fire4 = new Fire();
-        addObject(fire4,288,128);
+        addObject(new Ground(),105,488);
+        addObject(new Ground(),321,488);
+        addObject(new Ground(),536,488);
+        addObject(new Ground(),754,488);
+        addObject(new Ground(),882,488);
+        addObject(new Ground(),420,340);
+        addObject(new Ground(),790,420);//nivel 1 3
+        addObject(new Ground(),110,170);//Ultimo izquierda
+        addObject(new Ground(),315,170);
+        addObject(new Ground(),635,274);//El que está antes de caer
+        addObject(new Ground(),230,386);
+        addObject(new HomoSapines(),50,350);
+       
+        addObject(new Fire(),37,123);
+        addObject(new Fire(),352,447);
+        addObject(new Fire(),559,332);
+        addObject(new Fire(),288,128);
+        
+        addObject(new Tool(),158,349);
+        addObject(new Tool(),223,215);
+        addObject(new Tool(),284,349);
+        addObject(new Tool(),235,349);
+        addObject(new Tool(),405,450);
+        addObject(new Tool(),629,448);
+        addObject(new Tool(),94,129);
+        addObject(new Tool(),638,396);
+        addObject(new Tool(),582,449);
+        addObject(new Tool(),587,394);
+        addObject(new Tool(),533,447);
+        addObject(new Tool(),470,449);
+        addObject(new Tool(),491,395);
+        addObject(new Tool(),431,395);
+        addObject(new Tool(),200,349);
+        addObject(new Tool(),143,135);
+        addObject(new Tool(),193,133);
+        addObject(new Tool(),241,136);
     }
-
-
-    public Level2() 
-    {    
-        NextLevelImplementation();
-        prepare();
-    }
-  
 }
--- Level3.java
+++ Level3.java
@@ -15,68 +15,30 @@ public class Level3 extends General
     
     private void prepare()
     {
-        Building_1 building_1 = new Building_1();
-        addObject(building_1,62,370);
+        addObject(new Building_1(),62,370);
+        addObject(new Building_2(),170,411);
+        addObject(new Building_1(),492,371);
+        addObject(new Building_2(),600,413);
+        addObject(new Building_1(),705,493);
+        addObject(new Building_2(),809,412);
 
-        Building_2 building_2 = new Building_2();
-        addObject(building_2,170,411);
-
-        Building_1 building_12 = new Building_1();
-        addObject(building_12,492,371);
-
-        Building_2 building_22 = new Building_2();
-        addObject(building_22,600,413);
-
-        Building_1 building_13 = new Building_1();
-        addObject(building_13,705,493);
-
-        Building_2 building_23 = new Building_2();
-        addObject(building_23,809,412);
-
-        Human human = new Human();
-        addObject(human,27,189);
+        addObject(new Human(),27,189);
         
-        Book book = new Book();
-        addObject(book,451,224);
-        
-        Book book2 = new Book();
-        addObject(book2,573,305);
-        
-        Book book3 = new Book();
-        addObject(book3,661,345);
-        
-        Brain brain = new Brain();
-        addObject(brain,706,346);
-        
-        Brain brain2 = new Brain();
-        addObject(brain2,745,344);
-        
-        Brain brain3 = new Brain();
-        addObject(brain3,495,228);
-        
-        Brain brain4 = new Brain();
-        addObject(brain4,141,309);
-        
-        Book book4 = new Book();
-        addObject(book4,196,303);
-
-        Book book5 = new Book();
-        addObject(book5,611,307);
-        
-        Brain brain5 = new Brain();
-        addObject(brain5,406,61);
-        
-        Book book6 = new Book();
-        addObject(book6,254,38);
-        
-        Brain brain6 = new Brain();
-        addObject(brain6,639,43);
-        
-        Brain brain7 = new Brain();
-        addObject(brain7,249,478);
-        
-        Book book7 = new Book();
-        addObject(book7,250,433);
+        addObject(new Book(),451,224);
+        addObject(new Book(),573,305);
+        addObject(new Book(),661,345);
+        addObject(new Book(),250,433);
+        addObject(new Book(),196,303);
+        addObject(new Book(),611,307);
+        addObject(new Book(),254,38);
+    
+        addObject(new Brain(),706,346);
+        addObject(new Brain(),745,344);
+        addObject(new Brain(),495,228);
+        addObject(new Brain(),141,309);
+        addObject(new Brain(),406,61);
+        addObject(new Brain(),639,43);
+        addObject(new Brain(),249,478);     
     }
     
     public void createTrump(int quantity) {
